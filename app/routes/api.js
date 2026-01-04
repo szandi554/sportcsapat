32d3df2323b6219fadedc65113c223d03a23f6e8
szandi554 <szandi554@freemail.hu>
2026-01-04 21:11:14 +0100

    Add header comments to api.js
    Added file header comments including author, copyright, and license information.

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,3 +1,14 @@
+/*
+* File: api.js
+* Author: Siegenthaler Alexandra 
+* Copyright: 2026, Siegenthaler Alexandra 
+* Group: Szoft II/2/E
+* Date: 2026-01-01
+* Github: Siegenthaler Alexandra
+* Licenc: MIT
+*/
+
+
 import Router from 'express'
 const router = Router()
 
