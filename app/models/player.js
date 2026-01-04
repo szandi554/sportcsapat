351b63d46e939d6f2225aa0566484811bb0a8de4
szandi554 <szandi554@freemail.hu>
2026-01-04 21:13:22 +0100

    Add header comments to player.js

--- app/models/player.js
+++ app/models/player.js
@@ -1,3 +1,13 @@
+/*
+* File: player.js
+* Author: Siegenthaler Alexandra 
+* Copyright: 2026, Siegenthaler Alexandra 
+* Group: Szoft II/2/E
+* Date: 2026-01-01
+* Github: Siegenthaler Alexandra
+* Licenc: MIT
+*/
+
 import { DataTypes } from 'sequelize'
 import sequelize from '../database/database.js'
 
